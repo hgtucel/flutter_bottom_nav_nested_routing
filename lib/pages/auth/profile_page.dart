5d16a09ca7f7b4a16917e042f860e086c9fde406
hgtucel <tucelhg@gmail.com>
2023-02-21 17:36:28 +0300

    navigation routing added

--- lib/pages/auth/profile_page.dart
+++ lib/pages/auth/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bottom_nav_nested_routing/core/routes/router.gr.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -12,7 +14,16 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(child: Text("PROFILE PAGE")),
+      body: Center(child: Column(
+        children: [
+          TextButton(onPressed: (){
+            context.router.push(const EditProfileRoute());
+          }, child: const Text("EDIT PROFILE")),
+          TextButton(onPressed: (){
+            context.router.push(const EditPasswordRoute());
+          }, child: const Text("EDIT PASSWORD"))
+        ],
+      )),
     );
   }
 }
--- lib/pages/message/message_list.dart
+++ lib/pages/message/message_list.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bottom_nav_nested_routing/core/routes/router.gr.dart';
 
 class MessageListPage extends StatefulWidget {
   const MessageListPage({super.key});
@@ -11,7 +13,13 @@ class _MessagePageState extends State<MessageListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){
+            context.router.push(const AddMessageRoute());
+          }, icon: const Icon(Icons.add))
+        ],
+      ),
       body: const Center(child: Text("MESSAGE LIST PAGE")),
     );
   }
